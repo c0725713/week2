d084ecb474b71dea9e013e22c42f3f0e50735f8d
dhillon12 <46795501+dhillon12@users.noreply.github.com>
2019-01-17 17:01:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jashan_week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("bulldog");
+            dogs.Add("collie");
+            dogs.Add("Retriever");
+            //foreach (var dog in dogs)
+            //{
+            //    Console.WriteLine(dog + "");
+            // }
+            // Console.WriteLine(dogs[0]);
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach (int i in a1)
+            {
+                Console.WriteLine(i + "");
+            }
+        }
+    }
+}
